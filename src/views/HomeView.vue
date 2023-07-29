add83b7e1cb040fa1ab0d67dca219fc54937490d
Tarek-khwaiss <tarekonguitar@hotmail.com>
2023-07-29 19:09:28 +0300

    updated eslint and prettier settings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <h1>{{ heading1 }}</h1>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from "vue";
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  data() {
+    return {
+      heading1: "test",
+    };
   },
-})
-export default class HomeView extends Vue {}
+});
 </script>
+
+<style lang="scss" scoped></style>
